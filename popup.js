2e22a8a1717ddb709b409ed0575a194fe3f0cf72
Alexander Ozerski <a.ozerski2@mail.ru>
2024-07-03 12:55:05 +0300

    popup_close-button_added

--- popup.js
+++ popup.js
@@ -4,19 +4,21 @@ const BODY_FIXED_CLASSNAME = 'body_fixed';
 const bodyNode = document.querySelector('body');
 const popupNode = document.querySelector('.js-popup');
 const btnOpenNode = document.querySelector('.js-btn');
-const popupContentNode = document.querySelector('.js-popup__content')
+const popupContentNode = document.querySelector('.popup__content'); // обновлено, чтобы соответствовать вашему CSS
 const btnCloseNode = document.querySelector('.js-popup__close-btn');
 
 btnOpenNode.addEventListener('click', togglePopup);
-btnCloseNode.addEventListener('click', togglePopup);
+btnCloseNode.addEventListener('click', (event) => {
+    event.stopPropagation(); // предотвращает распространение события
+    togglePopup();
+});
 
 popupNode.addEventListener('click', (event) => {
-    const isClickOutsideContent = !event.composedPath().includes(popupContentNode)
-
-    if (isClickOutsideContent) {
+    const isClickInsideContent = event.composedPath().includes(popupContentNode);
+    if (!isClickInsideContent) {
         togglePopup();
     }
-})
+});
 
 function togglePopup() {
     popupNode.classList.toggle(POPUP_OPENED_CLASSNAME);
